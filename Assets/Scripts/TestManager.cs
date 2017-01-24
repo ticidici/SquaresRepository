47f1a00e36b5a2e6bc75fbe2cf793a811028bbcb
Nicolás Acosta <nicolasacostap@gmail.com>
2017-01-24 12:13:31 +0100

    Afegit TestManager. Eliminat arxius generats pel .exe.
    - TestManager: Simula el GameManager. Nomes esta per provar el input.
    - Eliminat els vertex que es guardaven en Square.
    - Modificat com es controla el jugador. Ja no es fa us de la variable "_isDominant", en SuperSquare, sino del tag.
    - El test esta configurat per pantalles que tinguin un aspect ratio de 16:9.

---
+++ Assets/Scripts/TestManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestManager : MonoBehaviour {
+
+    public SuperSquare _SuperSquarePrefab;
+    public Color[] _colors;    
+
+    void Awake()
+    {
+        Cursor.visible = false;
+        int i = 0;
+        Debug.Log(Input.GetJoystickNames().Length);
+       // foreach (var player in Input.GetJoystickNames())
+      //  {
+           // Debug.Log(player);
+            SuperSquare aux = Instantiate(_SuperSquarePrefab, new Vector2(Random.Range(-6,6), Random.Range(-3.5f, 3.5f)), Quaternion.identity) as SuperSquare;
+            aux.name = "P1";
+            aux.IdPlayer = 0;
+            aux.SetColor(_colors[0]);
+            aux.transform.parent = GameObject.Find("Test1").transform;
+     //   }
+
+        for (i = 1; i < 4; i++)
+        {
+            aux = Instantiate(_SuperSquarePrefab, new Vector2(Random.Range(-6, 6), Random.Range(-3.5f, 3.5f)), Quaternion.identity) as SuperSquare;
+            aux.IdPlayer = i;
+            aux.SetColor(_colors[i]);
+            aux.GetComponent<ControllerPlayer>().enabled = false;            
+            int auxTag = i + 1;
+            aux.name = "P" + auxTag;
+            aux.tag = "Player"+ auxTag;
+            aux.transform.parent = GameObject.Find("Test1").transform;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
