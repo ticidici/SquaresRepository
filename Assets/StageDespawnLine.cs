dbce6dd07cfcdcd41e83639a1a9f44aea000e51c
Daniel Tomas <ticidici@yahoo.es>
2017-02-17 03:04:57 +0100

    Aconseguir scroll infinit Hi ha un trigger a dalt de tot que desactiva el quadrant que li arriba i li diu al spawn point de baix de tot que l'activi a la seva posició. Evidentment quan fan respawn a vegades deixen espai, així que caldrà enganxar-los bé cada cop que s'activen. També molaria fer-ho procedural, fent spawn de nous quadrant a la part de baix, en comptes d'agafar el que ve de dalt. De moment estant utilitzant l'script MoveUp per moure's, probablement es canviará.

---
+++ Assets/StageDespawnLine.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageDespawnLine : MonoBehaviour {
+
+    private StageSpawnPoint _spawnPoint;
+
+	// Use this for initialization
+	void Awake () {
+        _spawnPoint = transform.parent.GetComponentInChildren<StageSpawnPoint>();
+        if (!_spawnPoint) { Debug.LogError("Falta un spawn point compartint pare amb la despawn line", this); }
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Entering Trigger");
+        GameObject incomingObject = collider.gameObject;
+        incomingObject.SetActive(false);
+        _spawnPoint.RespawnGameObject(incomingObject);
+    }
+}
---
+++ Assets/StageSpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageSpawnPoint : MonoBehaviour {
+
+    public void RespawnGameObject(GameObject objectToRespawn)
+    {
+        objectToRespawn.transform.position = transform.position;
+        objectToRespawn.SetActive(true);
+    }
+}
