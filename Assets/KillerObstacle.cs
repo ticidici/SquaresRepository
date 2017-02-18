7510a56310c088427fffd29ae050248717dc4efd
Daniel Tomas <ticidici@yahoo.es>
2017-02-18 02:51:28 +0100

    Retocs després del merge perquè tot vagi guay.

--- Assets/KillerObstacle.cs
+++ Assets/KillerObstacle.cs
@@ -8,11 +8,14 @@ public class KillerObstacle : MonoBehaviour {
     void Awake()
     {
         ParticleSystem ps = GetComponent<ParticleSystem>();
-        var col = ps.colorOverLifetime;
-        col.enabled = true;
-        Gradient grad = new Gradient();
-        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(GetComponent<Renderer>().material.color, 0.0f), new GradientColorKey(GetComponent<Renderer>().material.color, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, .17f), new GradientAlphaKey(0.0f, 1.0f) });
-        col.color = grad;
+        if (ps)
+        {
+            var col = ps.colorOverLifetime;
+            col.enabled = true;
+            Gradient grad = new Gradient();
+            grad.SetKeys(new GradientColorKey[] { new GradientColorKey(GetComponent<Renderer>().material.color, 0.0f), new GradientColorKey(GetComponent<Renderer>().material.color, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, .17f), new GradientAlphaKey(0.0f, 1.0f) });
+            col.color = grad;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
