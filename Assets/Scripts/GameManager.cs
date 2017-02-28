a9d23233eb99274db83d1df2d8277fe517bf24ea
Daniel Tomas <ticidici@yahoo.es>
2017-02-28 20:17:04 +0100

    El game manager sap les ids dels jugadors que queden en joc. Ara el game manager dóna un boost de punts al que queda viu últim. La idea és que ajudi molt a la victòria final, però que es pugui guanyar també pillant puntuació a la pantalla. Sería tipu pillar una snitch a Quidditch, que sol donar la victoria, però no sempre.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 
 public class GameManager : MonoBehaviour {
 
     public static int _numberOfStartingPlayers = 0;
     public static int _currentNumberOfPlayers = 0;
-
     public static GameManager _instance;
+
+    public int _winnerReward = 1500;
+
     private bool _isGameFinished = false;
+    private List<int> _currentPlayersIds = new List<int>();
 
     private void Awake()//singleton time
     {
@@ -27,6 +31,7 @@ public class GameManager : MonoBehaviour {
     public void AddPlayerToGame(int id)//TODO Usar esta id para, más tarde darle puntos extra a quien quede último
     {
         _numberOfStartingPlayers += 1;
+        _currentPlayersIds.Add(id);
         _currentNumberOfPlayers = _numberOfStartingPlayers;
         Debug.Log("Number of polygons: " + _numberOfStartingPlayers);
     }
@@ -34,10 +39,15 @@ public class GameManager : MonoBehaviour {
     public void PolygonKilled(int id)
     {
         _currentNumberOfPlayers -= 1;
+
+        _currentPlayersIds.Remove(id);
+
         Debug.Log("Number of polygons: " + _currentNumberOfPlayers + " out of " + _numberOfStartingPlayers);
         if (_currentNumberOfPlayers < 2 && !_isGameFinished)
         {
             _isGameFinished = true;
+            RewardWinner();
+            _currentPlayersIds.Clear();
             Invoke("ShowResultsScreen", 0.8f);
         }
     }
@@ -47,6 +57,11 @@ public class GameManager : MonoBehaviour {
         SceneManager.LoadScene("Results Screen");
         _isGameFinished = false;
         CancelInvoke();
+    }
+
+    private void RewardWinner()
+    {
+        ScoreManager.AddToScore(_winnerReward, _currentPlayersIds[0]);//Pressuposa que només queda un polygon
     }
   
 }
