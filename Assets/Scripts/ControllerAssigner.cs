3d5119f9aadef5019e8206aaacaf666087a81f1c
Daniel Tomas <ticidici@yahoo.es>
2017-03-27 20:12:45 +0200

    Es pot afegir un dispositiu a la partida apretant A en un mando connectat, des del menú. Amb B treus el dispositiu de la partida. Encara no es pot controlar quants quadrats apareixen a la següent pantalla segons el nombre de jugadors connectats. La idea seria també poder decidir el color i forma del polygon amb el que vols jugar.

--- Assets/Scripts/ControllerAssigner.cs
+++ Assets/Scripts/ControllerAssigner.cs
@@ -6,7 +6,12 @@ using InControl;
 public class ControllerAssigner : MonoBehaviour {
 
     List<InputDevice> _assignedDevices = new List<InputDevice>();
-	
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
 	void Update () {
         InputDevice device = InputManager.ActiveDevice;
 
@@ -36,8 +41,19 @@ public class ControllerAssigner : MonoBehaviour {
             {
                 Debug.Log("Other button pressed or unknown button");
             }
-        }
-
-        
+        }   
 	}
+
+    public InputDevice GetDevice(int playerId)
+    {
+        if (playerId <= _assignedDevices.Count)
+        {
+            return _assignedDevices[playerId - 1];
+        }
+        else
+        {
+            Debug.LogWarning("There's more players than controllers.");
+            return null;
+        }
+    }
 }
--- Assets/Scripts/SquareController.cs
+++ Assets/Scripts/SquareController.cs
@@ -5,12 +5,10 @@ public class SquareController : MonoBehaviour {
 
     [Range(1, 4)]
     public int _playerNumber = 1;
-    public int _deviceIndex;//no hacer esto, tener una referencia a InputDevice
 
+    private ControllerAssigner _controllerAssigner;
+    private InputDevice _thisController;
     private IControllable _polygonModel; // Canviar nom (tots sabem que aquest comentari mai es borrarà ni es farà res al respecte)
-    private string _horizontalAxis = "Horizontal ";
-    private string _verticalAxis = "Vertical ";
-    private string _magnetButton = "Magnet ";
 
     void Awake()
     {
@@ -20,42 +18,22 @@ public class SquareController : MonoBehaviour {
     
     void Update()
     {
+        if (_thisController == null) { return; }
         MovementInputBindings();
         OtherInputBindings();
     }
 
     public void SetPlayerController(int id)
     {
+        _controllerAssigner = FindObjectOfType<ControllerAssigner>();
+        if (_controllerAssigner == null) { Debug.LogError("No Controller assigner found!"); }
         _playerNumber = id;
-        switch (id)
-        {
-            case 1:
-                _horizontalAxis = string.Concat(_horizontalAxis, "1");
-                _verticalAxis = string.Concat(_verticalAxis, "1");
-                _magnetButton = string.Concat(_magnetButton, "1");
-                break;
-            case 2:
-                _horizontalAxis = string.Concat(_horizontalAxis, "2");
-                _verticalAxis = string.Concat(_verticalAxis, "2");
-                _magnetButton = string.Concat(_magnetButton, "2");
-                break;
-            case 3:
-                _horizontalAxis = string.Concat(_horizontalAxis, "3");
-                _verticalAxis = string.Concat(_verticalAxis, "3");
-                _magnetButton = string.Concat(_magnetButton, "3");
-                break;
-            case 4:
-                _horizontalAxis = string.Concat(_horizontalAxis, "4");
-                _verticalAxis = string.Concat(_verticalAxis, "4");
-                _magnetButton = string.Concat(_magnetButton, "4");
-                break;
-        }
+        _thisController = _controllerAssigner.GetDevice(id);
     }
     
     private void OtherInputBindings()
     {
-        //if (Input.GetButtonDown(_magnetButton))
-        if(InputManager.Devices[_deviceIndex].Action1)
+        if(_thisController.Action1)
         {
             _polygonModel.UseMagnet();
         }
@@ -65,13 +43,9 @@ public class SquareController : MonoBehaviour {
     {
         float x = 0;
         float y = 0;
-        //x = Input.GetAxis(_horizontalAxis);
-        //y = Input.GetAxis(_verticalAxis);
-        x = InputManager.Devices[_deviceIndex].LeftStickX;
-        y = InputManager.Devices[_deviceIndex].LeftStickY;
  
-        x = InputManager.ActiveDevice.LeftStickX;
-        y = InputManager.ActiveDevice.LeftStickY;
+        x = _thisController.LeftStickX;
+        y = _thisController.LeftStickY;
 
         _polygonModel.Move(x, y);
     }
