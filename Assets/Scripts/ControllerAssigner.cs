1c880e1ecb58e9a96c0b1eba478d32e028b5348a
Daniel Tomas <ticidici@yahoo.es>
2017-04-02 23:47:48 +0200

    Al menú principal es pot elegir el color y forma del quadrat amb que vols jugar. Els colors s'eliminen de la llista quan s'assignen i es recuperen quan un polygon canvia, així que tots tenen sempre colors diferents. Encara s'ha d'implementar que tot es traslladi correctament a la pantalla de joc.

--- Assets/Scripts/ControllerAssigner.cs
+++ Assets/Scripts/ControllerAssigner.cs
@@ -5,29 +5,52 @@ using InControl;
 
 public class ControllerAssigner : MonoBehaviour {
 
-    List<InputDevice> _assignedDevices = new List<InputDevice>();
+    public GameObject _previewPolygonPrefab;
+
+    private List<InputDevice> _assignedDevices = new List<InputDevice>();
+    private List<PreviewPolygon> _previewPolygons = new List<PreviewPolygon>();
+    private List<Color> _colorsToAssign = new List<Color>();
 
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        _colorsToAssign.Add(Color.red);
+        _colorsToAssign.Add(Color.blue);
+        _colorsToAssign.Add(Color.green);
+        _colorsToAssign.Add(Color.yellow);
+        _colorsToAssign.Add(Color.cyan);
     }
 
 	void Update () {
         InputDevice device = InputManager.ActiveDevice;
 
         if (device.AnyButton.WasPressed)
-        {
-            if (device.Action2.WasPressed)
-            {
-                Debug.Log("Removing controller: " + device.Name);
-                _assignedDevices.Remove(device);
-                return;
-            }
+        {  
             foreach (InputDevice assignedDevice in _assignedDevices)
             {
-
                 if (device == assignedDevice)
                 {
+                    int index = _assignedDevices.IndexOf(device);
+                    PreviewPolygon polygon = _previewPolygons[index];
+                    if (device.Action2.WasPressed)
+                    { 
+                        
+                        _colorsToAssign.Add(polygon.ReturnColor());
+                        _previewPolygons.Remove(polygon);
+                        Destroy(polygon.gameObject);
+                        Debug.Log("Removing controller: " + device.Name);
+                        _assignedDevices.Remove(device);
+                        return;
+                    }
+                    if (device.Action3.WasPressed)
+                    {
+                        _colorsToAssign.Add(polygon.ReturnColor());
+                        polygon.AssignColor(GetNewPreviewColor());
+                    }
+                    if (device.Action4.WasPressed)
+                    {
+                        polygon.ChangeShape();
+                    }
                     Debug.Log("Already assigned device: " + device.Name);
                     return;
                 } 
@@ -36,6 +59,12 @@ public class ControllerAssigner : MonoBehaviour {
             {
                 Debug.Log("A button pressed. Adding device: " + device.Name);
                 _assignedDevices.Add(device);
+
+                GameObject polygon = Instantiate(_previewPolygonPrefab);
+                PreviewPolygon script = polygon.GetComponent<PreviewPolygon>();
+                script.AssignController(device);
+                script.AssignColor(GetNewPreviewColor());
+                _previewPolygons.Add(script);
             }
             else
             {
@@ -55,5 +84,12 @@ public class ControllerAssigner : MonoBehaviour {
             Debug.LogWarning("There's more players than controllers.");
             return null;
         }
+    }
+
+    private Color GetNewPreviewColor()
+    {
+        Color color = _colorsToAssign[0];
+        _colorsToAssign.RemoveAt(0);
+        return color;
     }
 }
---
+++ Assets/Scripts/PreviewPolygon.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PreviewPolygon : MonoBehaviour {
+
+    private enum Shape
+    {
+        Square,
+        Triangle,
+        NumberOfShapes
+    };
+
+    public float _xForce = 17f;
+    public float _yForce = 16f;
+
+    private InputDevice _thisController;
+    private Color _thisColor;
+    private Shape _thisShape = Shape.Square;
+    private GameObject _squareChild;
+    private GameObject _triangleChild;
+    private SpriteRenderer _squareRenderer;
+    private SpriteRenderer _triangleRenderer;
+    private Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        foreach (Transform child in transform)
+        {
+            if (child.name == "Square")
+            {
+                _squareChild = child.gameObject;
+            }
+            else if (child.name == "Triangle")
+            {
+                _triangleChild = child.gameObject;
+            }
+        }
+        _squareRenderer = _squareChild.GetComponent<SpriteRenderer>();
+        _triangleRenderer = _triangleChild.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (_thisController == null) { return; }
+        MovementInputBindings();
+    }
+
+    public void AssignController(InputDevice device)
+    {
+        _thisController = device;
+    }
+
+    public void ChangeShape()
+    {
+        _triangleChild.SetActive(false);
+        _squareChild.SetActive(false);
+        _thisShape++;
+        if (_thisShape >= Shape.NumberOfShapes)
+        {
+            _thisShape = 0;
+        }
+        if (_thisShape == Shape.Square)
+        {
+            _squareChild.SetActive(true);
+        }
+        else if (_thisShape == Shape.Triangle)
+        {
+            _triangleChild.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Out of range shape", this);
+        }
+    }
+
+    public Color AssignColor(Color color)
+    {
+        Color aux = _thisColor;
+        _thisColor = color;
+        _triangleRenderer.color = _thisColor;
+        _squareRenderer.color = _thisColor;
+        return aux;
+    }
+
+    public Color ReturnColor()
+    {
+        return _thisColor;
+    }
+
+    void MovementInputBindings()
+    {
+        float x = 0;
+        float y = 0;
+        x = _thisController.LeftStickX;
+        y = _thisController.LeftStickY;
+
+        Vector2 forceVector = new Vector2(x, y);
+        forceVector.Normalize();
+        forceVector.x *= Time.fixedDeltaTime * _xForce;
+        forceVector.y *= Time.fixedDeltaTime * _yForce;
+        _rigidbody.AddForceAtPosition(forceVector, transform.position, ForceMode2D.Impulse);
+    }
+}
